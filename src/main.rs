09fdd9de3cbdb473fef29675fc443d382525656f
nkraetzschmar <nkraetzschmar@users.noreply.github.com>
2021-05-14 18:15:53 +0200

    refactor: move circuit for y=x^5 and y=x^{-1} to own function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use bellman::{
 	Field,
 	PrimeField,
 	Engine,
+	Variable,
 	Circuit,
 	ConstraintSystem,
 	SynthesisError,
@@ -21,51 +22,68 @@ use bellman::{
 	}
 };
 
-pub struct CubeDemo<E: Engine> {
+pub struct InvPow5<E: Engine> {
 	pub x: Option<E::Fr>,
 }
 
-impl <E: Engine> Circuit<E> for CubeDemo<E> {
+fn pow5<E: Engine, CS: ConstraintSystem<E>>(mut cs: CS, x_val: &Option<E::Fr>, x: &Variable) -> Result<(Option<E::Fr>, Variable), SynthesisError> {
+	let x_2_val = x_val.map(|mut x| { x.square(); x });
+	let x_2 = cs.alloc(|| "x_2", || x_2_val.ok_or(SynthesisError::AssignmentMissing))?;
+	cs.enforce(
+		|| "x_2 = x * x",
+		|lc| lc + *x,
+		|lc| lc + *x,
+		|lc| lc + x_2,
+	);
+
+	let x_4_val = x_2_val.map(|mut x| { x.square(); x });
+	let x_4 = cs.alloc(|| "x_4", || x_4_val.ok_or(SynthesisError::AssignmentMissing))?;
+	cs.enforce(
+		|| "x_4 = x_2 * x_2",
+		|lc| lc + x_2,
+		|lc| lc + x_2,
+		|lc| lc + x_4,
+	);
+
+	let x_5_val = x_4_val.and_then(|mut x| x_val.as_ref().map(|y| { x.mul_assign(y); x }));
+	let x_5 = cs.alloc(|| "x_5", || x_5_val.ok_or(SynthesisError::AssignmentMissing))?;
+	cs.enforce(
+		|| "x_5 = x * x_4",
+		|lc| lc + *x,
+		|lc| lc + x_4,
+		|lc| lc + x_5,
+	);
+
+	Ok((x_5_val, x_5))
+}
+
+fn inv<E: Engine, CS: ConstraintSystem<E>>(mut cs: CS, x_val: &Option<E::Fr>, x: &Variable) -> Result<(Option<E::Fr>, Variable), SynthesisError> {
+	let x_inv_val = x_val.and_then(|x| x.inverse());
+	let x_inv = cs.alloc(|| "x_inv", || x_inv_val.ok_or(SynthesisError::AssignmentMissing))?;
+	cs.enforce(
+		|| "x * x_inv = 1",
+		|lc| lc + *x,
+		|lc| lc + x_inv,
+		|lc| lc + CS::one(),
+	);
+
+	Ok((x_inv_val, x_inv))
+}
+
+impl <E: Engine> Circuit<E> for InvPow5<E> {
 	fn synthesize<CS: ConstraintSystem<E>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
 		let x_val = self.x;
 		let x = cs.alloc(|| "x", || x_val.ok_or(SynthesisError::AssignmentMissing))?;
 
-		let x_2_val = x_val.map(|mut x| { x.square(); x });
-		let x_2 = cs.alloc(|| "x_2", || x_2_val.ok_or(SynthesisError::AssignmentMissing))?;
-		cs.enforce(
-			|| "x_2 = x * x",
-			|lc| lc + x,
-			|lc| lc + x,
-			|lc| lc + x_2,
-		);
+		let (x_5_val, x_5) = pow5(cs.namespace(|| "pow5"), &x_val, &x)?;
+		let (x_5_inv_val, x_5_inv) = inv(cs.namespace(|| "inv"), &x_5_val, &x_5)?;
 
-		let x_4_val = x_2_val.map(|mut x| { x.square(); x });
-		let x_4 = cs.alloc(|| "x_4", || x_4_val.ok_or(SynthesisError::AssignmentMissing))?;
+		let out = cs.alloc_input(|| "out", || x_5_inv_val.ok_or(SynthesisError::AssignmentMissing))?;
 		cs.enforce(
-			|| "x_4 = x_2 * x_2",
-			|lc| lc + x_2,
-			|lc| lc + x_2,
-			|lc| lc + x_4,
-		);
-
-		let x_5_val = x_4_val.and_then(|mut x| x_val.as_ref().map(|y| { x.mul_assign(y); x }));
-		let x_5 = cs.alloc(|| "x_5", || x_5_val.ok_or(SynthesisError::AssignmentMissing))?;
-		cs.enforce(
-			|| "x_5 = x * x_4",
-			|lc| lc + x,
-			|lc| lc + x_4,
-			|lc| lc + x_5,
-		);
-
-		let out_val = x_5_val.and_then(|x| x.inverse());
-		let tmp = x_5_val.and_then(|mut x| out_val.as_ref().map(|y| { x.mul_assign(y); x }));
-		println!("{:?} {:?} {:?}", x_5_val, out_val, tmp);
-		let out = cs.alloc_input(|| "out", || out_val.ok_or(SynthesisError::AssignmentMissing))?;
-		cs.enforce(
-			|| "x_5 * out = 1",
-			|lc| lc + x_5,
-			|lc| lc + out,
+			|| "x_5_inv = out",
+			|lc| lc + x_5_inv,
 			|lc| lc + CS::one(),
+			|lc| lc + out,
 		);
 
 		Ok(())
@@ -74,7 +92,7 @@ impl <E: Engine> Circuit<E> for CubeDemo<E> {
 
 fn main(){
 	let rng = &mut thread_rng();
-	let c = CubeDemo::<Bls12> { x: None };
+	let c = InvPow5::<Bls12> { x: None };
 	let params = generate_random_parameters(c, rng).unwrap();
 
 	let pvk = prepare_verifying_key(&params.vk);
@@ -87,9 +105,8 @@ fn main(){
 		x_5.mul_assign(x);
 		x_5.inverse()
 	});
-	println!("{:?} {:?}", x, x_5_inv);
 
-	let c = CubeDemo::<Bls12> { x: x };
+	let c = InvPow5::<Bls12> { x: x };
 	let proof = create_random_proof(c, &params, rng).unwrap();
 
 	let correct = verify_proof(&pvk, &proof, &[x_5_inv.unwrap()]).unwrap();
