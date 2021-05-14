eae4b305a14d9d2edc5830e7a9a2c817f5ae4ad7
nkraetzschmar <nkraetzschmar@users.noreply.github.com>
2021-05-14 19:34:44 +0200

    generic pow constraint function for const exponent (using square & multiply algorithm)

--- src/main.rs
+++ src/main.rs
@@ -26,35 +26,45 @@ pub struct InvPow5<E: Engine> {
 	pub x: Option<E::Fr>,
 }
 
-fn pow5<E: Engine, CS: ConstraintSystem<E>>(mut cs: CS, x_val: &Option<E::Fr>, x: &Variable) -> Result<(Option<E::Fr>, Variable), SynthesisError> {
-	let x_2_val = x_val.map(|mut x| { x.square(); x });
-	let x_2 = cs.alloc(|| "x_2", || x_2_val.ok_or(SynthesisError::AssignmentMissing))?;
-	cs.enforce(
-		|| "x_2 = x * x",
-		|lc| lc + *x,
-		|lc| lc + *x,
-		|lc| lc + x_2,
-	);
+fn pow<E: Engine, CS: ConstraintSystem<E>>(n: u32, mut cs: CS, x_val: &Option<E::Fr>, x: &Variable) -> Result<(Option<E::Fr>, Variable), SynthesisError> {
+	match n {
+		0 => { Ok((Some(E::Fr::one()), CS::one())) }
+		_ => {
+			let mut x_n_val = *x_val;
+			let mut x_n = *x;
 
-	let x_4_val = x_2_val.map(|mut x| { x.square(); x });
-	let x_4 = cs.alloc(|| "x_4", || x_4_val.ok_or(SynthesisError::AssignmentMissing))?;
-	cs.enforce(
-		|| "x_4 = x_2 * x_2",
-		|lc| lc + x_2,
-		|lc| lc + x_2,
-		|lc| lc + x_4,
-	);
+			let len = 31 - n.leading_zeros();
+			for i in (0..len).rev() {
+				let x_2_val = x_n_val.map(|mut x| { x.square(); x });
+				let x_2 = cs.alloc(|| "x_2", || x_2_val.ok_or(SynthesisError::AssignmentMissing))?;
+				cs.enforce(
+					|| "x_2 = x_n * x_n",
+					|lc| lc + x_n,
+					|lc| lc + x_n,
+					|lc| lc + x_2,
+				);
 
-	let x_5_val = x_4_val.and_then(|mut x| x_val.as_ref().map(|y| { x.mul_assign(y); x }));
-	let x_5 = cs.alloc(|| "x_5", || x_5_val.ok_or(SynthesisError::AssignmentMissing))?;
-	cs.enforce(
-		|| "x_5 = x * x_4",
-		|lc| lc + *x,
-		|lc| lc + x_4,
-		|lc| lc + x_5,
-	);
+				if n & (1 << i) != 0 {
+					let x_3_val = x_2_val.and_then(|mut x| x_val.as_ref().map(|y| { x.mul_assign(y); x }));
+					let x_3 = cs.alloc(|| "x_3", || x_3_val.ok_or(SynthesisError::AssignmentMissing))?;
+					cs.enforce(
+						|| "x_3 = x * x_2",
+						|lc| lc + *x,
+						|lc| lc + x_2,
+						|lc| lc + x_3,
+					);
 
-	Ok((x_5_val, x_5))
+					x_n_val = x_3_val;
+					x_n = x_3;
+				} else {
+					x_n_val = x_2_val;
+					x_n = x_2;
+				}
+			}
+
+			Ok((x_n_val, x_n))
+		}
+	}
 }
 
 fn inv<E: Engine, CS: ConstraintSystem<E>>(mut cs: CS, x_val: &Option<E::Fr>, x: &Variable) -> Result<(Option<E::Fr>, Variable), SynthesisError> {
@@ -75,7 +85,7 @@ impl <E: Engine> Circuit<E> for InvPow5<E> {
 		let x_val = self.x;
 		let x = cs.alloc(|| "x", || x_val.ok_or(SynthesisError::AssignmentMissing))?;
 
-		let (x_5_val, x_5) = pow5(cs.namespace(|| "pow5"), &x_val, &x)?;
+		let (x_5_val, x_5) = pow(5, cs.namespace(|| "pow5"), &x_val, &x)?;
 		let (x_5_inv_val, x_5_inv) = inv(cs.namespace(|| "inv"), &x_5_val, &x_5)?;
 
 		let out = cs.alloc_input(|| "out", || x_5_inv_val.ok_or(SynthesisError::AssignmentMissing))?;
@@ -98,13 +108,8 @@ fn main(){
 	let pvk = prepare_verifying_key(&params.vk);
 
 	let x = Fr::from_str("3");
-	let x_5_inv = x.as_ref().and_then(|x| {
-		let mut x_5 = *x;
-		x_5.square();
-		x_5.square();
-		x_5.mul_assign(x);
-		x_5.inverse()
-	});
+	let x_5 = x.map(|x| x.pow(&[5]));
+	let x_5_inv = x_5.and_then(|x| x.inverse());
 
 	let c = InvPow5::<Bls12> { x: x };
 	let proof = create_random_proof(c, &params, rng).unwrap();
